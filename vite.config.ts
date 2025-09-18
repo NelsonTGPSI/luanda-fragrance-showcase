2fd748660a6d0433553bec53649d0b0bed9d9b46
NelsonTGPSI <nelsonalunogpsi@gmail.com>
2025-09-18 12:16:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/luanda-fragrance-showcase/", // <- Adiciona esta linha
   server: {
     host: "::",
     port: 8080,
